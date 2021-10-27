05ef2237f871ba545655e39fd58ca9d7d7e47492
TBoileau <t-boileau@email.com>
2021-10-28 01:01:16 +0200

    test: memoization technique

--- tests/functional/knapsack_dataset_0_test.py
+++ tests/functional/knapsack_dataset_0_test.py
@@ -7,16 +7,17 @@ from lib.knapsack.item import Item
 from lib.knapsack.knapsack_resolver_interface import KnapsackResolverInterface
 from lib.reader.reader_interface import ReaderInterface
 from src.app_kernel import AppKernel
-from src.knapsack.dynamic_programming_knapsack import DynamicProgrammingKnapsack
-from src.knapsack.greedy_knapsack import GreedyKnapsack
-from src.knapsack.naive_recursive import NaiveRecursiveKnapsack
+from src.knapsack.dynamic_programming import DynamicProgramming
+from src.knapsack.greedy import Greedy
+from src.knapsack.memoization_technique import MemoizationTechnique
+from src.knapsack.naive_recursive import NaiveRecursive
 
 
 def test_resolve_knapsack_dataset_0_with_greedy():
     kernel: Kernel = AppKernel()
     kernel.run()
     container: ContainerInterface = kernel.container
-    knapsack: KnapsackResolverInterface = container.get(GreedyKnapsack)
+    knapsack: KnapsackResolverInterface = container.get(Greedy)
     reader: ReaderInterface = container.get(ReaderInterface)
 
     data = reader.read(os.path.join(os.getcwd(), "fixtures", "dataset_0.csv"))
@@ -44,7 +45,7 @@ def test_resolve_knapsack_dataset_0_with_dynamic_programming():
     kernel: Kernel = AppKernel()
     kernel.run()
     container: ContainerInterface = kernel.container
-    knapsack: KnapsackResolverInterface = container.get(DynamicProgrammingKnapsack)
+    knapsack: KnapsackResolverInterface = container.get(DynamicProgramming)
     reader: ReaderInterface = container.get(ReaderInterface)
 
     data = reader.read(os.path.join(os.getcwd(), "fixtures", "dataset_0.csv"))
@@ -70,7 +71,7 @@ def test_resolve_knapsack_dataset_0_with_naive_recursive():
     kernel: Kernel = AppKernel()
     kernel.run()
     container: ContainerInterface = kernel.container
-    knapsack: KnapsackResolverInterface = container.get(NaiveRecursiveKnapsack)
+    knapsack: KnapsackResolverInterface = container.get(NaiveRecursive)
     reader: ReaderInterface = container.get(ReaderInterface)
 
     data = reader.read(os.path.join(os.getcwd(), "fixtures", "dataset_0.csv"))
@@ -91,3 +92,31 @@ def test_resolve_knapsack_dataset_0_with_naive_recursive():
     for item in items:
         total_weight += item.weight
     assert 50000 >= total_weight
+
+
+def test_resolve_knapsack_dataset_0_with_memoization_technique():
+    kernel: Kernel = AppKernel()
+    kernel.run()
+    container: ContainerInterface = kernel.container
+    knapsack: KnapsackResolverInterface = container.get(MemoizationTechnique)
+    reader: ReaderInterface = container.get(ReaderInterface)
+
+    data = reader.read(os.path.join(os.getcwd(), "fixtures", "dataset_0.csv"))
+
+    assert len(data) == 20
+
+    for row in data:
+        action_price: int = int(float(row['price']) * 100)
+        if action_price <= 0:
+            continue
+        profit: int = int(action_price * (float(row['profit']) / 100))
+        knapsack.add(Item(row['name'], action_price, profit))
+
+    items, total = knapsack.resolve(50000)
+
+    assert len(items) == 10
+    total_weight = 0
+    for item in items:
+        total_weight += item.weight
+    assert 50000 >= total_weight
+

bf793e32633cb2a70fdecb97184cdbde545fa06a
TBoileau <t-boileau@email.com>
2021-10-27 16:32:02 +0200

    test: knapsack with dataset_1.csv

--- src/app_kernel.py
+++ src/app_kernel.py
@@ -1,8 +1,14 @@
 """Imported modules/packages"""
+from lib.dependency_injection.container_interface import ContainerInterface
 from lib.kernel import Kernel
+from lib.reader.reader_interface import ReaderInterface
+from src.reader.csv_reader import CsvReader
 
 
 class AppKernel(Kernel):
     """
     AppKernel class
     """
+
+    def build(self, container: ContainerInterface):
+        container.alias(ReaderInterface, CsvReader)

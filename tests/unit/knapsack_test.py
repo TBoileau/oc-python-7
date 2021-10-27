be0f0de728cc9cb2a54f128b9563926cee8f8a58
Thomas Boileau <t-boileau@email.com>
2021-10-28 00:35:42 +0200

    Naive recursive (#29)
    * feat: naive recursive approach (#27)
    
    * test: naive recursive approach
    
    * fix: remove unnecessary path

--- tests/unit/knapsack_test.py
+++ tests/unit/knapsack_test.py
@@ -5,6 +5,7 @@ from lib.knapsack.item import Item
 from lib.knapsack.knapsack_resolver_interface import KnapsackResolverInterface
 from src.knapsack.dynamic_programming_knapsack import DynamicProgrammingKnapsack
 from src.knapsack.greedy_knapsack import GreedyKnapsack
+from src.knapsack.naive_recursive import NaiveRecursiveKnapsack
 
 
 def test_resolve_greedy_knapsack():
@@ -24,6 +25,22 @@ def test_resolve_greedy_knapsack():
 
 def test_resolve_dynamic_programming_knapsack():
     knapsack: KnapsackResolverInterface = DynamicProgrammingKnapsack()
+    knapsack.add(Item('1', 1, 10))
+    knapsack.add(Item('2', 5, 50))
+    knapsack.add(Item('3', 3, 20))
+    knapsack.add(Item('4', 2, 30))
+    knapsack.add(Item('5', 4, 60))
+    items, total = knapsack.resolve(11)
+
+    assert total == 140
+    assert len(items) == 3
+    assert items[0].name == '2'
+    assert items[1].name == '4'
+    assert items[2].name == '5'
+
+
+def test_resolve_naive_recursive_knapsack():
+    knapsack: KnapsackResolverInterface = NaiveRecursiveKnapsack()
     knapsack.add(Item('1', 1, 10))
     knapsack.add(Item('2', 5, 50))
     knapsack.add(Item('3', 3, 20))
